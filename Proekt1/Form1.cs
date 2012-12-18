1f55d964f161b2f11530ca8923b9b94ac4207cc9
Roman Gorlov <gorlov.r@orator.name>
2012-12-18 14:29:43 +0600

    Отрисовка крестика, добавление в список

--- Proekt1/Form1.cs
+++ Proekt1/Form1.cs
@@ -11,6 +11,7 @@ namespace Proekt1
 {
     public partial class Form1 : Form
     {
+        List<Cross> Shapes = new List<Cross>();
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +19,37 @@ namespace Proekt1
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y);
+            this.Text = Convert.ToString(e.X) + " | " + Convert.ToString(e.Y);
         }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Shapes.Add(new Cross(e.X, e.Y));
+            this.Refresh();
+        }
+        public class Cross
+        {
+            int X, Y;
+            Pen p = new Pen(Color.Red);
+            public Cross(int _X, int _Y)
+            {
+                X = _X; Y = _Y;
+            }
+            public void DrawWith(Graphics g)
+            {
+                g.DrawLine(p, X - 4, Y - 4, X + 4, Y + 4);
+                g.DrawLine(p, X + 4, Y - 4, X - 4, Y + 4);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Cross f in Shapes)
+            {
+                f.DrawWith(e.Graphics);
+            }
+        }
+      
     }
+
 }
